40755f4996886d509b462d4114599000da5bdeb6
Adithak_99 <94104632+Adithaklm@users.noreply.github.com>
2022-07-27 00:58:34 +0530

    Update ping.py

--- plugins/admemes/ping.py
+++ plugins/admemes/ping.py
@@ -2,15 +2,109 @@
 Syntax: .ping"""
 
 import time
+import os
 import random
+import heroku3
+import requests
+import math
+import time
 from pyrogram import Client, filters
-from info import COMMAND_HAND_LER
+from info import COMMAND_HAND_LER, ADMINS
 from plugins.helper_functions.cust_p_filters import f_onw_fliter
 from plugins.Dyno.dyno import bot_status_cmd
+from database.users_chats_db import db
 
 #=====================================================
 BOT_START_TIME = time.time()
+CMD = ['.', '/']
+HEROKU_API_KEY = (os.environ.get("HEROKU_API_KEY", ""))
 #=====================================================
+
+@Client.on_message(filters.private & filters.user(ADMINS) & filters.command("dyno", CMD))         
+async def bot_status_cmd(client,message):
+    if HEROKU_API_KEY:
+        try:
+            server = heroku3.from_key(HEROKU_API_KEY)
+
+            user_agent = (
+                'Mozilla/5.0 (Linux; Android 10; SM-G975F) '
+                'AppleWebKit/537.36 (KHTML, like Gecko) '
+                'Chrome/80.0.3987.149 Mobile Safari/537.36'
+            )
+            accountid = server.account().id
+            headers = {
+            'User-Agent': user_agent,
+            'Authorization': f'Bearer {HEROKU_API_KEY}',
+            'Accept': 'application/vnd.heroku+json; version=3.account-quotas',
+            }
+
+            path = "/accounts/" + accountid + "/actions/get-quota"
+
+            request = requests.get("https://api.heroku.com" + path, headers=headers)
+
+            if request.status_code == 200:
+                result = request.json()
+
+                total_quota = result['account_quota']
+                quota_used = result['quota_used']
+
+                quota_left = total_quota - quota_used
+                
+                total = math.floor(total_quota/3600)
+                used = math.floor(quota_used/3600)
+                hours = math.floor(quota_left/3600)
+                minutes = math.floor(quota_left/60 % 60)
+                days = math.floor(hours/24)
+
+                usedperc = math.floor(quota_used / total_quota * 100)
+                leftperc = math.floor(quota_left / total_quota * 100)
+
+#---------text--------🔥
+
+                quota_details = f"""
+💫SERVER STATUS💫
+
+💠 ToTal dyno ➪ {total}hr 𝖿𝗋𝖾𝖾 𝖽𝗒𝗇𝗈!
+ 
+💠 Dyno used ➪ {used} 𝖧𝗈𝗎𝗋𝗌 ( {usedperc}% )
+        
+💠 Dyno remaining ➪ {hours} 𝖧𝗈𝗎𝗋𝗌 ( {leftperc}% )
+        
+💠 Approximate days ➪ {days} days left!"""
+
+#----------end---------💯
+
+            else:
+                quota_details = ""
+        except:
+            print("Check your Heroku API key")
+            quota_details = ""
+    else:
+        quota_details = ""
+
+    uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - BOT_START_TIME))
+
+    try:
+        t, u, f = shutil.disk_usage(".")
+        total = humanbytes(t)
+        used = humanbytes(u)
+        free = humanbytes(f)
+
+        disk = "\n**Disk Details**\n\n" \
+            f"> USED  :  {used} / {total}\n" \
+            f"> FREE  :  {free}\n\n"
+    except:
+        disk = ""
+
+    await message.reply_text(
+        "<u>💥 CURRENT STATUS OF YOUR BOT💥</u>\n\n"
+        "💫DB STATUS\n"
+        f"➪ 𝖡𝗈𝗍 𝖴𝗉𝗍𝗂𝗆𝖾: {uptime}\n"
+        f"{quota_details}"
+        f"{disk}",
+        quote=True,
+        parse_mode="html"
+    )
 
 
 # -- Constants -- #
@@ -29,5 +123,5 @@ async def ping(_, message):
     rm = await message.reply_text("...")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"🏓 <b>ᴘɪɴɢ</b> : <code>{time_taken_s:.3f} ms</code>\n\n{start_t}")
+    await rm.edit(f"🏓 <b>ᴘɪɴɢ</b> : <code>{time_taken_s:.3f} ms</code>\n\n{uptime}")
 
